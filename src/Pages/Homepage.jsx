16594ef08606bc397ab50e5a5ea60073ad6c1af1
Agu Gideon <agugideon409@gmail.com>
2025-06-05 13:42:37 +0100

    fix import and export issues

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,11 +1,11 @@
 import Navbar from "../Components/Navbar";
-import HeroSection from "../Components/Herosection";
+import Hero from "../Components/Hero";
 
 const Homepage = () => {
   return (
     <div>
       <Navbar />
-      <HeroSection />
+      <Hero />
     </div>
   );
 };
